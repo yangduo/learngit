e91f47777bac79c68cd018e8a31583e68728718f
username <yangduo0330@thundersoft.com>
2017-04-05 17:07:06 +0800

    wrote a hello file

---
+++ hello.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+    > File Name: hello.c
+    > Author: LYD
+    > Mail: yangduo0330@thundersoft.com 
+    > Created Time: 2017年04月05日 星期三 17时05分29秒
+ ************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+	printf("hello thundersoft !");
+	return 0;
+}
+
